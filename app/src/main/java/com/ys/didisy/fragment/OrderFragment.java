374057b2fcdd762661ff04755edac3e6847c7c8a
YangTsai <h1991y1226y>
2017-03-14 14:45:39 +0800

    引入百度地图

--- app/src/main/java/com/ys/didisy/fragment/OrderFragment.java
+++ app/src/main/java/com/ys/didisy/fragment/OrderFragment.java
@@ -1,18 +1,23 @@
 package com.ys.didisy.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.ys.didisy.R;
+import com.ys.didisy.activity.MapActivity;
 
 /**
  * 下单
  */
-public class OrderFragment extends Fragment{
+public class OrderFragment extends Fragment implements View.OnClickListener{
 	public View view;
+	private TextView tv_map;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,8 +28,18 @@ public class OrderFragment extends Fragment{
 	}
 
 	private void initView() {
-
-
+		tv_map = (TextView) view.findViewById(R.id.tv_map);
+		tv_map.setOnClickListener(this);
 	}
 
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()){
+			case R.id.tv_map:
+				Intent intent = new Intent(getActivity(), MapActivity.class);
+				startActivity(intent);
+				break;
+		}
+	}
 }
