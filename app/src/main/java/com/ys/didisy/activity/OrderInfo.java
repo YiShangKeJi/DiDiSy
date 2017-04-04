fd5ca3d6b209070cefc929052ef97273d556ac7a
YangTsai <305502158@qq.com>
2017-04-04 23:33:05 +0800

    订单界面

---
+++ app/src/main/java/com/ys/didisy/activity/OrderInfo.java
@@ -0,0 +1,20 @@
+package com.ys.didisy.activity;
+
+import android.os.Bundle;
+
+import com.ys.didisy.R;
+
+/**
+ * Created by ASUS on 2017/4/4.
+ */
+
+public class OrderInfo extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.);
+
+    }
+}
--- app/src/main/java/com/ys/didisy/fragment/OrderFragment.java
+++ app/src/main/java/com/ys/didisy/fragment/OrderFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ys.didisy.R;
@@ -15,31 +16,56 @@ import com.ys.didisy.activity.MapActivity;
 /**
  * 下单
  */
-public class OrderFragment extends Fragment implements View.OnClickListener{
-	public View view;
-	private TextView tv_map;
+public class OrderFragment extends Fragment implements View.OnClickListener {
+    public View view;
+    private TextView tv_map;
+    private LinearLayout ll_small;
+    private LinearLayout ll_middle;
+    private LinearLayout ll_big;
+    private LinearLayout ll_most;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
-		view = inflater.inflate(R.layout.fragment_order, null);
-		initView();
-		return view;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_order, null);
+        initView();
+        return view;
+    }
 
-	private void initView() {
-		tv_map = (TextView) view.findViewById(R.id.tv_map);
-		tv_map.setOnClickListener(this);
-	}
+    private void initView() {
+        tv_map = (TextView) view.findViewById(R.id.tv_map);
+        ll_small = (LinearLayout) view.findViewById(R.id.ll_small);
+        ll_middle = (LinearLayout) view.findViewById(R.id.ll_middle);
+        ll_big = (LinearLayout) view.findViewById(R.id.ll_big);
+        ll_most = (LinearLayout) view.findViewById(R.id.ll_most);
+        ll_small.setOnClickListener(this);
+        ll_middle.setOnClickListener(this);
+        ll_big.setOnClickListener(this);
+        ll_most.setOnClickListener(this);
+        tv_map.setOnClickListener(this);
+    }
 
 
-	@Override
-	public void onClick(View view) {
-		switch (view.getId()){
-			case R.id.tv_map:
-				Intent intent = new Intent(getActivity(), MapActivity.class);
-				startActivity(intent);
-				break;
-		}
-	}
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_map:
+                Intent intent = new Intent(getActivity(), MapActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.ll_small:
+
+                break;
+            case R.id.ll_middle:
+
+                break;
+            case R.id.ll_big:
+
+                break;
+            case R.id.ll_most:
+
+                break;
+
+        }
+    }
 }
